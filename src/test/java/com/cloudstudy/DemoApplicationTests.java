de72f61cb89688f0d20554b1feb4a39b4b76d5e2
sunlei <365243317@qq.com>
2018-02-05 16:03:45 +0800

    fix

--- src/test/java/com/cloudstudy/DemoApplicationTests.java
+++ src/test/java/com/cloudstudy/DemoApplicationTests.java
@@ -1,8 +1,11 @@
 package com.cloudstudy;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -12,5 +15,12 @@ public class DemoApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
-
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate;
+	@Test
+	public void test() throws Exception {
+		// 保存字符串
+		stringRedisTemplate.opsForValue().set("aaa", "111");
+		Assert.assertEquals("111", stringRedisTemplate.opsForValue().get("aaa"));
+	}
 }
